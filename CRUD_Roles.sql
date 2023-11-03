afa8043afa8748ac3fb68d56c9c9316a9d8eb7dc
IsisRP <90296577+IsisRP@users.noreply.github.com>
2023-11-03 00:41:59 -0600

    CRUD_Roles.sql

--- CRUD_Roles.sql
+++ CRUD_Roles.sql
@@ -11,7 +11,7 @@ BEGIN
 
     IF @modo = 1
     BEGIN
-        -- Validar si la descripciÛn del rol ya existe
+        -- Validar si la descripci√≥n del rol ya existe
         IF NOT EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[roles] WHERE descripcionRol = @descripcionRol)
         BEGIN
             -- Insertar un nuevo rol
@@ -21,23 +21,31 @@ BEGIN
         END
         ELSE
         BEGIN
-            PRINT 'La descripciÛn del rol ya existe. No se ha creado un nuevo rol.';
+            PRINT 'La descripci√≥n del rol ya existe. No se ha creado un nuevo rol.';
         END
     END
     ELSE IF @modo = 2
     BEGIN
-        -- Validar si la descripciÛn del rol ya existe antes de actualizar
+        -- Validar si la descripci√≥n del rol ya existe antes de actualizar
         IF NOT EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[roles] WHERE descripcionRol = @descripcionRol)
         BEGIN
-            -- Actualizar un rol existente
-            UPDATE [ProximaGen].[proximagen].[roles]
-            SET descripcionRol = @descripcionRol
-            WHERE idRol = @idRol;
-            SET @creado = 1;
+			-- Validar si el ID de Rol existe antes de eliminar
+			IF EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[roles] WHERE idRol = @idRol)
+			BEGIN
+				-- Actualizar un rol existente
+				UPDATE [ProximaGen].[proximagen].[roles]
+				SET descripcionRol = @descripcionRol
+				WHERE idRol = @idRol;
+				SET @creado = 1;
+			END
+			ELSE
+			BEGIN
+				PRINT 'El ID de Rol no existe. No se ha realizado la eliminaci√≥n.';
+			END
         END
         ELSE
         BEGIN
-            PRINT 'La descripciÛn del rol ya existe. No se ha realizado la actualizaciÛn.';
+            PRINT 'La descripci√≥n del rol ya existe. No se ha realizado la actualizaci√≥n.';
         END
     END
     ELSE IF @modo = 3
@@ -52,25 +60,24 @@ BEGIN
         END
         ELSE
         BEGIN
-            PRINT 'El ID de Rol no existe. No se ha realizado la eliminaciÛn.';
+            PRINT 'El ID de Rol no existe. No se ha realizado la eliminaci√≥n.';
         END
     END
     ELSE
     BEGIN
-        -- Manejar otros modos o valores de modo seg˙n sea necesario
-        -- Puedes agregar m·s casos o manejar errores aquÌ si es necesario
-        PRINT 'Modo no v·lido.';
+        -- Manejar otros modos o valores de modo seg√∫n sea necesario
+        -- Puedes agregar m√°s casos o manejar errores aqu√≠ si es necesario
+        PRINT 'Modo no v√°lido.';
     END
 
     IF @creado = 1
     BEGIN
-        PRINT 'La operaciÛn se ha completado con Èxito.';
+        PRINT 'La operaci√≥n se ha completado con √©xito.';
     END
 END;
 
-
 -- Ejemplo de llamada al procedimiento almacenado para insertar un nuevo Rol
-EXEC CUD_Roles 1, NULL, 'Profesor';
+EXEC CUD_Roles 1, NULL, 'Profesosr';
 
 -- Ejemplo de llamada al procedimiento almacenado para actualizar un Rol existente
 EXEC CUD_Roles 2, 1, 'Estudiante';
