7dacd4360efb0a34320e75da105d98f4e51e4d8f
IsisRP <90296577+IsisRP@users.noreply.github.com>
2023-11-03 00:40:44 -0600

    CRUD_Permisos.sql

--- CRUD_Permisos.sql
+++ CRUD_Permisos.sql
@@ -30,15 +30,23 @@ BEGIN
         -- Validar si el permiso ya existe antes de actualizar
         IF NOT EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[permisos] WHERE permiso = @permiso)
         BEGIN
-            -- Actualizar un permiso existente
-            UPDATE [ProximaGen].[proximagen].[permisos]
-            SET permiso = @permiso
-            WHERE idPermiso = @idPermiso;
-            SET @creado = 1;
+			-- Validar si el ID de Permiso existe antes de eliminar
+			IF EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[permisos] WHERE idPermiso = @idPermiso)
+			BEGIN
+				-- Actualizar un permiso existente
+				UPDATE [ProximaGen].[proximagen].[permisos]
+				SET permiso = @permiso
+				WHERE idPermiso = @idPermiso;
+				SET @creado = 1;
+			END
+			ELSE
+			BEGIN
+				PRINT 'El ID de Permiso no existe. No se ha realizado la actualizaci√≥n.';
+			END
         END
         ELSE
         BEGIN
-            PRINT 'El permiso ya existe. No se ha realizado la actualizaciÛn.';
+            PRINT 'El permiso ya existe. No se ha realizado la actualizaci√≥n.';
         END
     END
     ELSE IF @modo = 3
@@ -53,25 +61,24 @@ BEGIN
         END
         ELSE
         BEGIN
-            PRINT 'El ID de Permiso no existe. No se ha realizado la eliminaciÛn.';
+            PRINT 'El ID de Permiso no existe. No se ha realizado la eliminaci√≥n.';
         END
     END
     ELSE
     BEGIN
-        -- Manejar otros modos o valores de modo seg˙n sea necesario
-        -- Puedes agregar m·s casos o manejar errores aquÌ si es necesario
-        PRINT 'Modo no v·lido.';
+        -- Manejar otros modos o valores de modo seg√∫n sea necesario
+        -- Puedes agregar m√°s casos o manejar errores aqu√≠ si es necesario
+        PRINT 'Modo no v√°lido.';
     END
 
     IF @creado = 1
     BEGIN
-        PRINT 'La operaciÛn se ha completado con Èxito.';
+        PRINT 'La operaci√≥n se ha completado con √©xito.';
     END
 END;
 
-
 -- Ejemplo de llamada al procedimiento almacenado para insertar un nuevo Permiso
-EXEC CUD_Permisos 1, NULL, 'Permisos de tsi';
+EXEC CUD_Permisos 1, NULL, 'Permisos de profesor';
 
 -- Ejemplo de llamada al procedimiento almacenado para actualizar un Permiso existente
 EXEC CUD_Permisos 2, 1, 'Permiso de estudiante';
