f6bd4de64ee203977bb1b1da67227c25fa0a8d40
IsisRP <90296577+IsisRP@users.noreply.github.com>
2023-11-03 00:42:33 -0600

    CRUD_Tipo_Descuento.sql

--- CRUD_Tipo_Descuento.sql
+++ CRUD_Tipo_Descuento.sql
@@ -30,15 +30,23 @@ BEGIN
         -- Validar si el tipo de descuento ya existe antes de actualizar
         IF NOT EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[tipodescuento] WHERE tipoDescuento = @tipoDescuento)
         BEGIN
-            -- Actualizar un tipo de descuento existente
-            UPDATE [ProximaGen].[proximagen].[tipodescuento]
-            SET tipoDescuento = @tipoDescuento
-            WHERE idTipoDescuento = @idTipoDescuento;
-            SET @creado = 1;
+			-- Validar si el ID de Tipo de Descuento existe antes de eliminar
+			IF EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[tipodescuento] WHERE idTipoDescuento = @idTipoDescuento)
+			BEGIN
+				-- Actualizar un tipo de descuento existente
+				UPDATE [ProximaGen].[proximagen].[tipodescuento]
+				SET tipoDescuento = @tipoDescuento
+				WHERE idTipoDescuento = @idTipoDescuento;
+				SET @creado = 1;
+			END
+			ELSE
+			BEGIN
+				PRINT 'El ID de Tipo de Descuento no existe. No se ha realizado la eliminaci√≥n.';
+			END
         END
         ELSE
         BEGIN
-            PRINT 'El tipo de descuento ya existe. No se ha realizado la actualizaciÛn.';
+            PRINT 'El tipo de descuento ya existe. No se ha realizado la actualizaci√≥n.';
         END
     END
     ELSE IF @modo = 3
@@ -53,25 +61,24 @@ BEGIN
         END
         ELSE
         BEGIN
-            PRINT 'El ID de Tipo de Descuento no existe. No se ha realizado la eliminaciÛn.';
+            PRINT 'El ID de Tipo de Descuento no existe. No se ha realizado la eliminaci√≥n.';
         END
     END
     ELSE
     BEGIN
-        -- Manejar otros modos o valores de modo seg˙n sea necesario
-        -- Puedes agregar m·s casos o manejar errores aquÌ si es necesario
-        PRINT 'Modo no v·lido.';
+        -- Manejar otros modos o valores de modo seg√∫n sea necesario
+        -- Puedes agregar m√°s casos o manejar errores aqu√≠ si es necesario
+        PRINT 'Modo no v√°lido.';
     END
 
     IF @creado = 1
     BEGIN
-        PRINT 'La operaciÛn se ha completado con Èxito.';
+        PRINT 'La operaci√≥n se ha completado con √©xito.';
     END
 END;
 
-
 -- Ejemplo de llamada al procedimiento almacenado para insertar un nuevo Tipo de Descuento
-EXEC CUD_TipoDescuento 1, NULL, 'Descuento 30%';
+EXEC CUD_TipoDescuento 1, NULL, 'Descuento 25%';
 
 -- Ejemplo de llamada al procedimiento almacenado para actualizar un Tipo de Descuento existente
 EXEC CUD_TipoDescuento 2, 1, 'Descuento 15%';
