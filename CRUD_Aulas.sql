d30b80434812f434cc4f6e36b3196ba8bca1c20e
IsisRP <90296577+IsisRP@users.noreply.github.com>
2023-11-03 00:35:10 -0600

    Update and rename CRUD_AULAS.sql to CRUD_Aulas.sql

--- CRUD_Aulas.sql
+++ CRUD_Aulas.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE CUD_Aulas
 (
     @modo INT,
     @idAula INT,
-    @direccionAula NVARCHAR(50)
+    @direccionAula NVARCHAR(100)
 )
 AS
 BEGIN
@@ -13,7 +13,7 @@ BEGIN
 
     IF @modo = 1
     BEGIN
-        -- Validar si la direcciÛn del aula ya existe
+        -- Validar si la direcci√≥n del aula ya existe
         IF NOT EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[aulas] WHERE direccionAula = @direccionAula)
         BEGIN
             -- Insertar una nueva aula
@@ -23,23 +23,31 @@ BEGIN
         END
         ELSE
         BEGIN
-            PRINT 'La direcciÛn del aula ya existe. No se ha creado una nueva aula.';
+            PRINT 'La direcci√≥n del aula ya existe. No se ha creado una nueva aula.';
         END
     END
     ELSE IF @modo = 2
     BEGIN
-        -- Validar si la direcciÛn del aula ya existe antes de actualizar
+        -- Validar si la direcci√≥n del aula ya existe antes de actualizar
         IF NOT EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[aulas] WHERE direccionAula = @direccionAula)
         BEGIN
-            -- Actualizar un aula existente
-            UPDATE [ProximaGen].[proximagen].[aulas]
-            SET direccionAula = @direccionAula
-            WHERE idAula = @idAula;
-            SET @creado = 1;
+            -- Validar si el ID de Aula existe antes de actualizar
+            IF EXISTS (SELECT 1 FROM [ProximaGen].[proximagen].[aulas] WHERE idAula = @idAula)
+            BEGIN
+                -- Actualizar un aula existente
+                UPDATE [ProximaGen].[proximagen].[aulas]
+                SET direccionAula = @direccionAula
+                WHERE idAula = @idAula;
+                SET @creado = 1;
+            END
+            ELSE
+            BEGIN
+                PRINT 'El ID de Aula no existe. No se ha realizado la actualizaci√≥n.';
+            END
         END
         ELSE
         BEGIN
-            PRINT 'La direcciÛn del aula ya existe. No se ha realizado la actualizaciÛn.';
+            PRINT 'La direcci√≥n del aula ya existe. No se ha realizado la actualizaci√≥n.';
         END
     END
     ELSE IF @modo = 3
@@ -54,19 +62,19 @@ BEGIN
         END
         ELSE
         BEGIN
-            PRINT 'El ID de Aula no existe. No se ha realizado la eliminaciÛn.';
+            PRINT 'El ID de Aula no existe. No se ha realizado la eliminaci√≥n.';
         END
     END
     ELSE
     BEGIN
-        -- Manejar otros modos o valores de modo seg˙n sea necesario
-        -- Puedes agregar m·s casos o manejar errores aquÌ si es necesario
-        PRINT 'Modo no v·lido.';
+        -- Manejar otros modos o valores de modo seg√∫n sea necesario
+        -- Puedes agregar m√°s casos o manejar errores aqu√≠ si es necesario
+        PRINT 'Modo no v√°lido.';
     END
 
     IF @creado = 1
     BEGIN
-        PRINT 'La operaciÛn se ha completado con Èxito.';
+        PRINT 'La operaci√≥n se ha completado con √©xito.';
     END
 END;
 
